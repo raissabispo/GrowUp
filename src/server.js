1a20e784a4f4ce0cba2e9309969b9e87b01762b3
Raissa <uamr.ieadpe1@gmail.com>
2025-03-10 21:31:19 -0300

    Fundamentos de construções da API

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 import  http  from 'node:http'
 
 
+const users = []
 const server = http.createServer((req, res) =>{
-     return res.end('OPAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+     const {method, url } = req
+
+     if(method === 'GET' && url === '/users'){
+          return res
+          .setHeader('Content-type', 'aplication/json')
+          .end(JSON.stringify(users))
+     }
+     if (method === 'POST' && url === '/users'){
+          users.push({
+               id: 1,
+               name: 'Felipe Amâncio',
+               email: 'amancio@gmail.com'
+          })
+          return res.writeHead(201).end()
+     }
+     
+     return res.writeHead(404).end('Deu errado, amigo')
 })
 
 server.listen(3333)
