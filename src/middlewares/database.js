b746e48790cdbea4c002c4857ce43b2a0bcba3fe
Raissa <uamr.ieadpe1@gmail.com>
2025-03-25 16:31:23 -0300

    finaliza rotas da aplicação

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,38 +1,46 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
 import path from 'node:path';
 
+// Define o caminho absoluto para o arquivo db.json na raiz do projeto
 const databasePath = path.resolve(process.cwd(), 'db.json');
 
-
 export class Database {
-    #database = {}
- constructor(){
-  fs.readFile(databasePath, 'utf8').then(data =>{
-    this.#database = JSON.parse(data)
-  })
-  .catch(()=>{
-    this.#persist()
-  })
- }
-    #persist(){
-        fs.writeFile(databasePath, JSON.stringify(this.#database))
-    }
-  
-    select(table) {
-     const data = this.#database[table] ?? []
+  #database = {};
 
-     return data 
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
     }
-  
-    insert(table, data) {
-      // Verifica se a tabela existe, caso contrário, inicializa como um array vazio
-      if (!Array.isArray(this.#database[table])) {
-          this.#database[table] = [];
-      }
-      // Adiciona o item à tabela
-      this.#database[table].push(data);
-      // Persiste os dados no arquivo
-      this.#persist();
-      return data;
+
+    return data;
   }
+
+  insert(table, data) {
+    if (!Array.isArray(this.#database[table])) {
+      this.#database[table] = [];
+    }
+    this.#database[table].push(data);
+    this.#persist();
+    return data;
   }
+}
--- src/server.js
+++ src/server.js
@@ -1,31 +1,30 @@
-import  http  from 'node:http'
-import { randomUUID } from 'node:crypto';
-import { json } from './middlewares/json.js';
-import { Database } from '../src/middlewares/database.js';
+import http from 'node:http'
 
-const database = new Database()
+import { json } from './middlewares/json.js'
+import { routes } from './middlewares/routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
-let userId = 1; 
-const server = http.createServer(async(req, res) =>{
-     const {method, url } = req
-     await  json(req,res)
-     
-     if(method === 'GET' && url === '/users'){
-          const users = database.select('users')
-          return res.end(JSON.stringify(users))
-     }
-     if (method === 'POST' && url === '/users'){
-        const {name, email} = req.body
-        const user = {
-          id: randomUUID(),
-          name,
-          email,
-      }
-      database.insert('users', user);
-          return res.writeHead(201).end()
-     }
-     
-     return res.writeHead(404).end('Deu errado, amigo')
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
